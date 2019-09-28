3fff445347bb093718e12002bc4c0cb14fc2fb5a
Vadym <vkravchik@gmail.com>
2019-09-28 23:28:01 +0300

    add index.html and minify. Fix img path

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,55 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
 
-  module: {
-    rules: [
-      // SCSS Rules
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
+    module: {
+        rules: [
+            // SCSS Rules
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            //  Image Rules
+            {
+                test: /\.(jpg|png|jpeg)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "images/[name].[ext]",
+                    },
+                },
+            },
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('index.css'),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: './index.html',
+            filename: './index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            },
         })
-      },
-      //  Image Rules
-      {
-        test: /\.(jpg|png|jpeg)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "images/[name].[ext]",
-          },
-        },
-      },
-    ]
-  },
+    ],
 
-  plugins: [
-    new ExtractTextPlugin('index.css')
-  ],
-
-  watch: true
+    watch: true
 };
